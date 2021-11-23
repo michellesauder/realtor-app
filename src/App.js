2383d7b294286f0253d556cf852da80539effe83
Michelle Sauder <mgsauder@hotmail.com>
2021-11-23 13:00:45 -0800

    fixed routes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/listings">Listings</Link>
+            </li>
+            {/* <li>
+              <Link to="/users">Users</Link>
+            </li> */}
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/about">
+            {/* <About /> */}
+          </Route>
+          <Route path="/users">
+            {/* <Users /> */}
+          </Route>
+          <Route path="/">
+            {/* <Home /> */}
+          </Route>
+        </Routes>
+      </div>
+    </Router>
+    
   );
 }
 
