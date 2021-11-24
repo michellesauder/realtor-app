766264ff7f61b9741e7469d643426aff0f395a3f
Michelle Sauder <mgsauder@hotmail.com>
2021-11-24 10:58:44 -0800

    working on routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,65 @@
-import './App.css';
-import Header from '../src/Header/Header';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from "react-router-dom";
 import Listings from '../src/Listings/Listings';
+import AddListings from '../src/components/AddListings';
+import Header from '../src/Header/Header';
 
+const App = () => {
+  let routes = useRoutes([
+    { path: "/", element: <AppWrapper /> },
+    { path: "/listings", element: <Listings /> },
+    { path: "add-listings", element: <AddListings /> },
+  ]);
+  return routes;
+};
 
-function App() {
+const AppWrapper = () => {
   return (
-    <div>
-      <Header />
-      <Listings />
-    </div>
+    <Router>
+      <Header/>
+      <App />
+    </Router>
   );
-}
+};
 
-export default App;
+export default AppWrapper;
+
+
+
+// import './App.css';
+// import Header from '../src/Header/Header';
+// import Listings from '../src/Listings/Listings';
+// import AddListings from '../src/components/AddListings';
+// import React from "react";
+// import {
+//   Route,
+//   Routes
+// } from "react-router-dom";
+
+// export default function App() {
+//   return (
+//     <>
+//     <Header /> 
+//     <Routes>
+//       <Route path="/" element={<App />} />
+//       <Route path="listings" element={<Listings />} />
+//       <Route path="add-listings" element={<AddListings />} />
+//     </Routes>
+//     </>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Listings />
+//       {/* <AddListings /> */}
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/Listings/Listings.js
+++ src/Listings/Listings.js
@@ -1,5 +1,5 @@
 import './Listings.css';
-import AddListings from '../components/AddListings';
+
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
@@ -14,74 +14,74 @@ import CardMedia from '@mui/material/CardMedia';
 function Listings() {
   return (
       <Container maxWidth="sm">
-              <Box
-        sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        '& > :not(style)': {
-            m: 1,
-            width: 500,
-            height: 128,
-        },
-        }}
-        >
-        <Paper>
-            <Card sx={{ minWidth: 250 }}>
-                <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    Hanson Manor
-                </Typography>
-                <Typography variant="h5" component="div">
-                    Price: $500,000
-                </Typography>
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="/static/House_Tour_Liverman_3D6A3138_tour.0.jpg"
-                />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    Vancouver
-                </Typography>
-                <Typography variant="body2">
-                    B.C
-                    <br />
-                    {'This property is currently active'}
-                </Typography>
-                </CardContent>
-                <CardActions>
-                <Button size="small">Learn More</Button>
-                </CardActions>
-            </Card>
-            <Card sx={{ minWidth: 250 }}>
-                <CardContent>
-                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                    Gastown Place
-                </Typography>
-                <Typography variant="h5" component="div">
-                    Price: $320,000
-                </Typography>
-                <CardMedia
-                    component="img"
-                    height="194"
-                    image="/static/House_Tour_Liverman_3D6A3138_tour.0.jpg"
-                />
-                <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                    Vancouver
-                </Typography>
-                <Typography variant="body2">
-                    B.C
-                    <br />
-                    {'This property is currently active'}
-                </Typography>
-                </CardContent>
-                <CardActions>
-                <Button size="small">Learn More</Button>
-                </CardActions>
-            </Card>
-        </Paper>
-    </Box>
-
-      </Container>
+            <Box
+            sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            '& > :not(style)': {
+                m: 1,
+                width: 500,
+                height: 128,
+            },
+            }}
+            >
+            <Paper>
+                <Card sx={{ minWidth: 250 }}>
+                    <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Hanson Manor
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        Price: $500,000
+                    </Typography>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="/static/House_Tour_Liverman_3D6A3138_tour.0.jpg"
+                    />
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        Vancouver
+                    </Typography>
+                    <Typography variant="body2">
+                        B.C
+                        <br />
+                        {'This property is currently active'}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+                <Card sx={{ minWidth: 250 }}>
+                    <CardContent>
+                    <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Gastown Place
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        Price: $320,000
+                    </Typography>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image="/static/House_Tour_Liverman_3D6A3138_tour.0.jpg"
+                    />
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        Vancouver
+                    </Typography>
+                    <Typography variant="body2">
+                        B.C
+                        <br />
+                        {'This property is currently active'}
+                    </Typography>
+                    </CardContent>
+                    <CardActions>
+                    <Button size="small">Learn More</Button>
+                    </CardActions>
+                </Card>
+            </Paper>
+            </Box>
+        </Container>
+    // <AddListings />
   );
 }
 
