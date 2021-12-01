5ce18b46316139f9e71c92ae8720cea1e20e0d47
Michelle Sauder <mgsauder@hotmail.com>
2021-12-01 13:51:09 -0800

    updated messaging

--- src/Messaging/Messaging.jsx
+++ src/Messaging/Messaging.jsx
@@ -4,15 +4,54 @@ import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
+import InputUnstyled from '@mui/base/InputUnstyled';
+import { styled } from '@mui/system';
+import Button from '@mui/material/Button';
+
+const StyledInputElement = styled('input')`
+  width: 100%;
+  font-size: 1rem;
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: 400;
+  line-height: 1.4375em;
+  background: rgb(243, 246, 249);
+  border: 1px solid #e5e8ec;
+  border-radius: 10px;
+  padding: 6px 10px;
+  color: #20262d;
+  transition: width 300ms ease;
+  height: 200px;
+
+  &:hover {
+    background: #eaeef3;
+    border-color: #e5e8ec;
+  }
+
+  &:focus {
+    outline: none;
+    width: 100%;
+    transition: width 200ms ease-out;
+  }
+`;
+
+const CustomInput = React.forwardRef(function CustomInput(props, ref) {
+  return (
+    <InputUnstyled components={{ Input: StyledInputElement }} {...props} ref={ref} />
+  );
+});
+
 
 function Messaging(props) {
+    
     return (
         <React.Fragment>
             <CssBaseline />
             <Container maxWidth="sm">
-                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', display: 'flex', textAlign: 'center' }} >
-                    <img src="/static/"/>
-                <TextField  fullWidth id="filled-basic" label="Subject" variant="filled" />
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', display: 'flex', textAlign: 'center', flexDirection: 'column'}} >
+                    <img src="/static/email_mail_box_envelope_send_with_flag.jpeg" />
+                    <TextField  fullWidth id="filled-basic" label="Subject" variant="filled" />
+                    <CustomInput aria-label="Message" />
+                    <Button variant="contained">Submit</Button>
                 </Box>
             </Container>
         </React.Fragment>
