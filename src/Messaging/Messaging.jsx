e6484a4014998e70b59afdf444a10c1d0a9616eb
Michelle Sauder <mgsauder@hotmail.com>
2021-12-01 13:39:24 -0800

    updating messaging

--- src/Messaging/Messaging.jsx
+++ src/Messaging/Messaging.jsx
@@ -1,13 +1,21 @@
 import { Container } from '@mui/material';
 import React from 'react';
 import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function Messaging(props) {
     return (
-        <Container sm='md' style={{marginTop: `10vh`}}>
-            <TextField  id="outlined-basic" label="Subject" variant="outlined"/>
-           <textarea />
-        </Container>
+        <React.Fragment>
+            <CssBaseline />
+            <Container maxWidth="sm">
+                <Box sx={{ bgcolor: '#cfe8fc', height: '100vh', display: 'flex', textAlign: 'center' }} >
+                    <img src="/static/"/>
+                <TextField  fullWidth id="filled-basic" label="Subject" variant="filled" />
+                </Box>
+            </Container>
+        </React.Fragment>
     );
 }
 
